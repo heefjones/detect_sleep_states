85c7513cc24c69824ecc6478875d084c2790a5e1
heefjones <jimmyhj9@gmail.com>
2025-04-05 22:26:18 -0500

    finished prediction formatting for the test data

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 This [Kaggle competition](https://www.kaggle.com/competitions/child-mind-institute-detect-sleep-states) tasked me to use accelerometer data to predict sleep onset and wake-up events.
 
 ## Data
+
 ### 1. Series
 Contained accelerometer data from 277 unique users. Each row represented a reading for a 5-second timestep. Thus, each user had hundreds of thousands of rows. These logs contained 2 features: __z-angle__ (refers to the z-angle of the arm relative to the body's vertical axis) and __enmo__ (a standard measure of arm acceleration).
 - **Rows**: ~127.9 million.
 - **Null Values**: None.
+
 ### 2. Events
 Contained sleep onset and wake-up events for each user, with specific timestamps for each. Null values were recorded for the timestep if the user was not wearing the device (or the device died).
 - **Rows**: ~14.5 thousand.
@@ -14,19 +16,22 @@ Contained sleep onset and wake-up events for each user, with specific timestamps
 ## Feature Engineering
 From the original data, 3 features were used: hour, z-angle, and enmo.
 
-Then, z-angle and enmo were differenced for each row (5-second intervals) and these differences were used as features. Finally, mean, standard deviation, minimum, and maximum features of both z-angle and enmo were computed across [1, 3, 5, 7.5, 10, 12.5, 15, 20, 25, 30, 60, 120, 180, 240, 480] minute windows. The 2 differenced features were also aggregated across these windows. This resulted in __215__ final features.
-
-Features (except for "hour") were then z-score normalized by subtracting the mean and dividing by the standard deviation.
+Then, z-angle and enmo were differenced for each row (5-second intervals) and these differences were used as features. Finally, minimum, maximum, mean, and standard deviation features of both z-angle and enmo were computed across [1, 3, 5, 7.5, 10, 12.5, 15, 20, 25, 30, 60, 120, 180, 240, 480] minute (15 total) windows. The 2 differenced features were also aggregated across these windows. This resulted in __245__ final features.
 
 ## Modeling
 First, the data between onset and wake-up events was flagged with "1" to indicate sleep, while all other data was flagged with "0" to indicate awake. The binary classifier was trained to predict at every 5-second timestep whether the user was sleeping or awake.
 
-I first tested model performance on a random 10% subset (28 of the 277 user's data). 
+I first tested model performance on a random 10% subset (28 of the 277 user's data). A default XGBoost Classifier achieved 96% accuracy on this subset.
 
-The model did have a tendency to predict multiple onset and wake events in the same night (predicting many short bouts of sleep), but the competition rules explicitly state that "A single sleep period must be at least 30 minutes in length" and that "The longest sleep window during the night is the only one which is recorded". Thus, I pruned short sleep periods that were under 2 hours in duration and took the longest bout for each night.
+The model did have a tendency to predict multiple onset and wake events in the same night (predicting many short bouts of sleep), but the competition rules explicitly state that "A single sleep period must be at least 30 minutes in length" and that "The longest sleep window during the night is the only one which is recorded". Thus, I pruned short sleep periods that were under 30 minutes in duration and also combined windows that were within 30 minutes of one another. If there were still multiple windows relatively close in a single night, I took the longest bout for each night. Submissions were evaluated on the [average precision](https://scikit-learn.org/stable/modules/generated/sklearn.metrics.average_precision_score.html) of detected events, averaged over timestamp error tolerance thresholds, averaged over event classes. 
 
+Here were the final results on the training data:
 - **Model**: XGBoost Classifier  
-- **Results**: 
+- **Average Precision**: 0.71
+
+Here were the final competition results (test data):
+- **Model**: XGBoost Classifier  
+- **Average Precision**: 0.62
 
 ## Files
 - 📊 eda.ipynb – EDA and visualization.
--- helper.py
+++ helper.py
@@ -236,7 +236,7 @@ def label_data(df):
           .then(1)
           .otherwise(0)
           .cum_sum().over("series_id")
-          .alias("day"))
+          .alias("day")).cast(pl.UInt8)
 
     # do a cumulative sum of 'sleep_indicator' so that rows after an "onset" have cumsum > 0 until we hit a "wakeup"
     df = df.with_columns(pl.col("sleep_indicator").cum_sum().over("series_id").alias("sleep_cumsum"))
@@ -325,8 +325,7 @@ def train_val_split(series, events, n):
 
     # sample series ids
     train_ids = np.random.choice(series_ids, n_train, replace=False)
-    remaining_ids = [sid for sid in series_ids if sid not in train_ids]
-    val_ids = np.random.choice(remaining_ids, n_val, replace=False)
+    val_ids = np.random.choice([sid for sid in series_ids if sid not in train_ids], n_val, replace=False)
 
     # create train and val data
     data_train = series.filter(pl.col("series_id").is_in(train_ids))
@@ -355,41 +354,204 @@ def create_features(df):
     # convert to lazy frame for better performance
     lazy_df = df.lazy()
 
+    # define the rolling aggregation functions
+    agg_funcs = {"min":  lambda col, w: pl.col(col).rolling_min(w, min_samples=1, center=True).abs(), 
+                 "max":  lambda col, w: pl.col(col).rolling_max(w, min_samples=1, center=True).abs(), 
+                 "mean": lambda col, w: pl.col(col).rolling_mean(w, min_samples=1, center=True).abs(), 
+                 "std":  lambda col, w: pl.col(col).rolling_std(w, min_samples=1, center=True).fill_nan(0)}
+
     # list of windows in minutes
     windows = [1, 3, 5, 7.5, 10, 12.5, 15, 20, 25, 30, 60, 120, 180, 240, 480]
 
     # loop over each window duration, creating rolling features
-    for m in tqdm(windows, desc='Iterating through windows'):
-        # number of 5-sec intervals per window
+    for m in windows:
+        # multiply by 12 to convert 5-second steps to minutes
         window_size = int(m * 12)
 
-        # create rolling features for each column
-        for col in ['enmo', 'anglez']:
-            # rolling features for the original signal
-            lazy_df = lazy_df.with_columns([
-                pl.col(col).rolling_mean(window_size, min_samples=1, center=True).abs().alias(f'{col}_{m}m_mean'),
-                pl.col(col).rolling_max(window_size, min_samples=1, center=True).abs().alias(f'{col}_{m}m_max'),
-                pl.col(col).rolling_min(window_size, min_samples=1, center=True).abs().alias(f'{col}_{m}m_min'),
-                pl.col(col).rolling_std(window_size, min_samples=1, center=True).fill_nan(0).alias(f'{col}_{m}m_std')])
+        # create a list to hold the expressions for this window size
+        exprs = []
 
-            # rolling features for the signal's diff (captures volatility)
+        # create rolling features for 'anglez' and 'enmo'
+        for col in ['anglez', 'enmo']:
+            for stat, func in agg_funcs.items():
+                alias_name = f'{col}_{m}m_{stat}'
+                exprs.append(func(col, window_size).alias(alias_name).cast(pl.Int16))
+            
+            # difference features
             diff_col = f'{col}_diff'
-            lazy_df = lazy_df.with_columns([
-                pl.col(diff_col).rolling_mean(window_size, min_samples=1, center=True).abs().alias(f'{diff_col}_{m}m_mean'),
-                pl.col(diff_col).rolling_max(window_size, min_samples=1, center=True).abs().alias(f'{diff_col}_{m}m_max'),
-                pl.col(diff_col).rolling_min(window_size, min_samples=1, center=True).abs().alias(f'{col}_{m}m_min'),
-                pl.col(diff_col).rolling_std(window_size, min_samples=1, center=True).fill_nan(0).alias(f'{diff_col}_{m}m_std')])
+            for stat, func in agg_funcs.items():
+                alias_name = f'{diff_col}_{m}m_{stat}'
+                exprs.append(func(diff_col, window_size).alias(alias_name).cast(pl.UInt16))
+        
+        # add the rolling features
+        lazy_df = lazy_df.with_columns(exprs)
 
-    # collect the results back into a DataFrame
+    # collect the results back into a standard df
     return lazy_df.collect()
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+def normalize_features(features):
+    """
+    Normalize the features in the DataFrame.
 
+    Args:
+    - features (pl.DataFrame): DataFrame containing features.
+
+    Returns:
+    - (pl.DataFrame): DataFrame with normalized features.
+    """
+
+    # define cols to be normalized
+    non_norm_cols = ['series_id', 'step', 'date', 'day', 'asleep', 'hour']
+    norm_cols = [col for col in features.columns if col not in non_norm_cols]
+
+    # compute mean and std for each column as dictionaries
+    mean_dict = features.select(norm_cols).mean().to_dicts()[0]
+    std_dict = features.select(norm_cols).std().to_dicts()[0]
+
+    # normalize
+    features_norm = features.with_columns([((pl.col(col) - mean_dict[col]) / std_dict[col]) for col in norm_cols])
+
+    return features_norm
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+def batch_data(data, batch_size=1_000_000):
+    """
+    Create batches of data for training.
 
+    Args:
+    - data (pl.DataFrame): Data to be batched.
+    - batch_size (int): Size of each batch. Default is 1 million.
+
+    Returns:
+    - (generator): Yields batches of data.
+    """
+
+    # iterate through batches
+    for i in range(0, len(data), batch_size):
+        # get batch
+        batch = data[i:i + batch_size]
+
+        # generate features from the batch
+        yield create_features(batch)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+def predict(data, classifier):
+    """
+    Takes a time series of (containing features and labels) and a classifier and returns a formatted submission dataframe.
+
+    Args:
+    - data (pl.DataFrame): Contains series data and sleep events.
+    - classifier (sklearn.classifier): Trained classifier for prediction.
+
+    Returns:
+    - event_preds_df (pd.DataFrame): Contains predicted sleep events.
+    """
+    
+    # non-feature columns
+    non_feat_cols = ['series_id', 'step', 'date']
+
+    # get unique series_ids as a list
+    series_ids = data.select("series_id").unique().to_series().to_list()
+
+    # list to accumulate event predictions
+    event_preds = []
+
+    # iterate through series_ids
+    for sid in tqdm(series_ids, desc="Processing users"):
+        # filter the data for the current series id
+        user_data = data.filter(pl.col("series_id") == sid).sort("step")
+
+        # create features
+        features = create_features(user_data)
+
+        # define X
+        X = features.drop(non_feat_cols)
+
+        # get preds
+        preds = classifier.predict(X)
+        probs = classifier.predict_proba(X)[:, 1]
+        
+        # append step, date, and predictions to X
+        X = X.with_columns([pl.Series("step", features["step"]), pl.Series("date", features["date"]), pl.Series("pred", preds), pl.Series("prob", probs)])
+        
+        # calculate the difference in predictions to find changes
+        X = X.with_columns(pl.col("pred").diff().alias("pred_diff"))
+        
+        # extract the 'step' values where the change indicates an onset (0 -> 1) or wakeup (1 -> 0)
+        pred_onsets = X.filter(pl.col("pred_diff") > 0)["step"].to_list()
+        pred_wakeups = X.filter(pl.col("pred_diff") < 0)["step"].to_list()
+        
+        # process events if we have at least one onset and wakeup
+        if len(pred_onsets) > 0 and len(pred_wakeups) > 0:
+            # if first wakeup occurs before the first onset, drop it
+            if pred_wakeups[0] < pred_onsets[0]:
+                pred_wakeups = pred_wakeups[1:]
+            # if last onset occurs after the last wakeup, drop it
+            if pred_onsets and pred_wakeups and pred_onsets[-1] > pred_wakeups[-1]:
+                pred_onsets = pred_onsets[:-1]
+            
+            # create sleep segments only if the duration is at least 30 minutes
+            segments = [(onset, wakeup) for onset, wakeup in zip(pred_onsets, pred_wakeups) if (wakeup - onset) >= (30 * 12)]
+
+            # merge segments that are close together
+            merged_segments = []
+            current_start, current_end = segments[0]
+            for onset, wakeup in segments[1:]:
+                # merge segments that are separated by less than 2 consecutive hours
+                if onset - current_end < (120 * 12):
+                    current_end = wakeup
+                else:
+                    merged_segments.append((current_start, current_end))
+                    current_start, current_end = onset, wakeup
+            merged_segments.append((current_start, current_end))
+
+            # keep only the longest window per night
+            segments_by_night = {}
+            for onset, wakeup in merged_segments:
+                # get the date of the onset step
+                night_key = X.filter(pl.col("step") == onset).select(pl.col("date")).to_series()[0]
+
+                # get duration of the segment
+                duration = wakeup - onset
+
+                # check if current segment is longer than the existing one
+                if night_key not in segments_by_night or duration > segments_by_night[night_key]["duration"]:
+                    # update the segment for this night
+                    segments_by_night[night_key] = {"onset": onset, "wakeup": wakeup, "duration": duration}
+
+            # iterate through segments and get scores
+            for night_key, seg in segments_by_night.items():
+                # get onset and wakeup times
+                onset_step, wakeup_step = seg["onset"], seg["wakeup"]
+
+                # record only the longest sleep window per night
+                sleep_segment = X.filter((pl.col("step") >= onset_step) & (pl.col("step") < wakeup_step))
+                score = sleep_segment.select(pl.col("prob")).mean().item()
+                
+                # get onset and wakeup dates
+                onset_date = X.filter(pl.col("step") == onset_step).select(pl.col("date")).to_series()[0]
+                wakeup_date = X.filter(pl.col("step") == wakeup_step).select(pl.col("date")).to_series()[0]
+                
+                # append events to the list
+                event_preds.append({"series_id": sid, "step": onset_step, "event": "onset", "score": score, "date": onset_date})
+                event_preds.append({"series_id": sid, "step": wakeup_step, "event": "wakeup", "score": score, "date": wakeup_date})
+
+    # create a pandas df for the preds
+    if event_preds:
+        event_preds_df = pd.DataFrame(event_preds)
+    else:
+        # create an empty DataFrame with the six required columns if no events were detected
+        event_preds_df = pd.DataFrame(columns=['series_id', 'step', 'event', 'score', 'date'])
+
+    # add 'row_id' col
+    event_preds_df['row_id'] = range(len(event_preds_df))
+
+    # reorder cols
+    return event_preds_df[['row_id', 'series_id', 'step', 'event', 'score', 'date']]
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
@@ -437,9 +599,9 @@ def plot_preds(data, preds, events):
         # plot vertical lines for events
         for _, event in day_events.iterrows():
             if event['event'] == 'onset':
-                ax.axvline(x=event['step'], color='green', linestyle='--', linewidth=1, label='onset')
+                ax.axvline(x=event['step'], color='green', linestyle='--', linewidth=2, label='onset')
             elif event['event'] == 'wakeup':
-                ax.axvline(x=event['step'], color='red', linestyle='--', linewidth=1, label='wakeup')
+                ax.axvline(x=event['step'], color='red', linestyle='--', linewidth=2, label='wakeup')
 
         # get the onset and wakeup preds for the day
         day_preds = preds.query('(step >= @min_step) & (step <= @max_step)')
@@ -447,11 +609,9 @@ def plot_preds(data, preds, events):
         # plot vertical lines for preds
         for _, event in day_preds.iterrows():
             if event['event'] == 'onset':
-                ax.axvline(x=event['step'], color='pink', linestyle='--', linewidth=1, label='pred_onset')
-                ax.annotate(f"{event['score']:.2f}", xy=(event['step'], event['score']), fontsize=8)
+                ax.axvline(x=event['step'], color='skyblue', linestyle='--', linewidth=2, label='pred_onset')
             elif event['event'] == 'wakeup':
-                ax.axvline(x=event['step'], color='skyblue', linestyle='--', linewidth=1, label='pred_wakeup')
-                ax.annotate(f"{event['score']:.2f}", xy=(event['step'], event['score']), fontsize=8)
+                ax.axvline(x=event['step'], color='pink', linestyle='--', linewidth=2, label='pred_wakeup')
 
         # set title
         ax.set_title(f"User {preds['series_id'].iloc[0]}: Night {i+1}", fontsize=14, fontweight="bold")
@@ -459,8 +619,8 @@ def plot_preds(data, preds, events):
         # custom legends
         legend_elements = [Line2D([0], [0], color='green', linestyle='--', label='onset'), 
                         Line2D([0], [0], color='red', linestyle='--', label='wakeup'), 
-                        Line2D([0], [0], color='pink', linestyle='--', label='pred_onset'), 
-                        Line2D([0], [0], color='skyblue', linestyle='--', label='pred_wakeup')]
+                        Line2D([0], [0], color='skyblue', linestyle='--', label='pred_onset'), 
+                        Line2D([0], [0], color='pink', linestyle='--', label='pred_wakeup')]
 
         # place legend above the plot with 4 columns
         fig.legend(handles=legend_elements, loc="upper center", ncol=4, fontsize=12, frameon=False, bbox_to_anchor=(0.5, 0.95))
@@ -468,6 +628,3 @@ def plot_preds(data, preds, events):
         # adjust layout so the legend doesn't overlap the plotting area
         fig.tight_layout(rect=[0, 0, 1, 0.90])
         plt.show()
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-
